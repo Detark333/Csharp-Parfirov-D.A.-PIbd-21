1d66a5c3e7130dde119b69a60d7d8edbb05b8dbd
Detark333 <denis_73007@mail.ru>
2020-05-28 09:29:07 +0400

    Changes

--- AbstractStore/AbstractJewerlyStoreView/Program.cs
+++ AbstractStore/AbstractJewerlyStoreView/Program.cs
@@ -1,22 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AbstractJewelryStoreView;
+using AbstractJewerlyStoreBusinessLogic.BuisnessLogic;
+using AbstractJewerlyStoreBusinessLogic.Interfaces;
+using AbstractStoreListImlement.Implements;
+using AbstractStoreListImplement.Implements;
+using System;
 using System.Windows.Forms;
+using Unity;
+using Unity.Lifetime;
 
 namespace AbstractJewerlyStoreView
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
+            var container = BuildUnityContainer();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(container.Resolve<FormMain>());
+        }
+        private static IUnityContainer BuildUnityContainer()
+        {
+            var currentContainer = new UnityContainer();
+            currentContainer.RegisterType<IJewerlyLogic, JewerlyLogic>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IOrderLogic, OrderLogic>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IProductLogic, ProductLogic>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<MainLogic>(new HierarchicalLifetimeManager());
+            return currentContainer;
         }
     }
 }
