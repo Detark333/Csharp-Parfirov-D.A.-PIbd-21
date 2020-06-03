ef9df22521d68512422120f1f6611b9a4816bb12
Detark333 <denis_73007@mail.ru>
2020-06-03 13:38:51 +0400

    Lab5

---
+++ AbstractStore/AbstractJewerlyStoreBusinessLogic/BindingModels/ClientBindingModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace AbstractJewerlyStoreBusinessLogic.BindingModels
+{
+    public class ClientBindingModel
+    {
+        [DataMember]
+        public int? Id { get; set; }
+        [DataMember]
+        public string FIO { get; set; }
+        [DataMember]
+        public string Login { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+    }
+}
