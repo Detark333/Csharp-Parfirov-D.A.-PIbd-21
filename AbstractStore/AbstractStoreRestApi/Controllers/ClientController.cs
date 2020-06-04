7b11bc934d8a03db853bd665efe572a5355c9eed
Detark333 <denis_73007@mail.ru>
2020-06-04 12:42:44 +0400

    changes

--- AbstractStore/AbstractStoreRestApi/Controllers/ClientController.cs
+++ AbstractStore/AbstractStoreRestApi/Controllers/ClientController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AbstractStoreRestApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ClientController : ControllerBase
     {
