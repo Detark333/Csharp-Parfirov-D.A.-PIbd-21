e0b7d7ab6acf0d768967826b18182ed14e2d1bad
Detark333 <denis_73007@mail.ru>
2020-06-10 12:40:02 +0400

    changes

--- AbstractStore/WebApplication2/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AbstractStoreRestApi.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string ProductName { get; set; }
-        public decimal Price { get; set; }
-    }
-}
