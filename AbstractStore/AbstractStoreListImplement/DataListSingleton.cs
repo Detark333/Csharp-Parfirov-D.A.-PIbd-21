b0155e0f3900a68f2fe1bba7fd4b5456e6d61d16
Detark333 <55380480+Detark333@users.noreply.github.com>
2020-07-02 12:21:28 +0400

    Update DataListSingleton.cs

--- AbstractStore/AbstractStoreListImplement/DataListSingleton.cs
+++ AbstractStore/AbstractStoreListImplement/DataListSingleton.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace AbstractJewerlyFileImplement
+namespace AbstractStoreListImplement
 {
     public class DataListSingleton
     {
