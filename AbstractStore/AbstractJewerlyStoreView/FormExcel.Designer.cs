44dd06232f7361d09fee69e8c6502f526460fe51
Detark333 <55380480+Detark333@users.noreply.github.com>
2020-09-02 01:35:33 +0400

    Update FormExcel.Designer.cs

--- AbstractStore/AbstractJewerlyStoreView/FormExcel.Designer.cs
+++ AbstractStore/AbstractJewerlyStoreView/FormExcel.Designer.cs
@@ -111,7 +111,7 @@
             this.Controls.Add(this.dateTimePickerFrom);
             this.Controls.Add(this.dataGridViewOrders);
             this.Name = "FormExcel";
-            this.Text = "FormExcel";
+            this.Text = "Report Excel";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrders)).EndInit();
             this.ResumeLayout(false);
 
