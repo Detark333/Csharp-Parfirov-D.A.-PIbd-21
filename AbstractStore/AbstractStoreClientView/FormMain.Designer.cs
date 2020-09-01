f598c1274bec930a4e37a01db918c4d1ccd16537
Detark333 <55380480+Detark333@users.noreply.github.com>
2020-09-02 01:51:22 +0400

    Update FormMain.Designer.cs

--- AbstractStore/AbstractStoreClientView/FormMain.Designer.cs
+++ AbstractStore/AbstractStoreClientView/FormMain.Designer.cs
@@ -84,7 +84,7 @@
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Главная форма";
+            this.Text = "Main";
             this.mainMenuStrip.ResumeLayout(false);
             this.mainMenuStrip.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ordersGridView)).EndInit();
